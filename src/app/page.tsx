5eaf90c0245e157440e67241262e8fddbc68015b
hetthakkar <het.thakkar809@gmail.com>
2023-09-26 01:02:19 -0400

    improve responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-start justify-between p-24'>
-      <h1 className='text-4xl font-bold text-center text-foreground'>
+    <main className='flex min-h-screen flex-col items-start justify-between p-12 md:p-24'>
+      <h1 className='text-2xl md:text-4xl font-bold text-center text-foreground'>
         Weather
       </h1>
       <SearchLocation />
